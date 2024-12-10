59878fa498040b3874b6ee4819233347ddfa27dd
Przemyslaw Siedlecki <przemyslaw.siedlecki@gmail.com>
2024-12-10 04:37:13 +0100

    Changed how we are dealing with threads

--- src/sensorsAPI.cpp
+++ src/sensorsAPI.cpp
@@ -11,7 +11,6 @@ SensorsAPI::SensorsAPI(const char* apiURL, const char* apiKey, const char* devic
   this->_apiURL = concatString;
   this->_apiKey = apiKey;
   this->_t1 = task1;
-  this->taskCreated = 0;
   this->_debug = false;
 }
 
@@ -20,125 +19,98 @@ void SensorsAPI::enableDebugging() {
 }
 void SensorsAPI::debug(String msg) {
   if (this->_debug) {
+    String coreID = String(xPortGetCoreID());
+    Serial.print("[CORE ");
+    Serial.print(coreID);
+    Serial.print("] ");
     Serial.println(msg);
   }
 }
 
-bool SensorsAPI::isRequestFinished() {
-  return this->_requestSent;
+void SensorsAPI::beginLoop() {
+  xTaskCreatePinnedToCore(
+    this->_mainLoop, /* Task function. */
+    "Task1",   /* name of task. */
+    10000,     /* Stack size of task */
+    (void *)this,      /* parameter of the task */
+    1,         /* priority of the task */
+    &_t1,    /* Task handle to keep track of created task */
+    0      /* pin task to core 0 */
+  );        
 }
 
-void SensorsAPI::deleteRequestTask() {
-  vTaskDelete(this->_t1);
-  this->_requestSent = false;
-  this->taskCreated = 0;
-  this->debug("Deleted send POST task");
-}
-
-void SensorsAPI::_sendAsyncPost(void* pvParameter) {
+void SensorsAPI::_mainLoop(void* pvParameter) {
   SensorsAPI* SensorsAPIInstance = (SensorsAPI *) pvParameter;
-  TaskHandle_t xHandle = NULL;
-  SensorsAPIInstance->debug("starting _sendAsyncPost running on core ");
+  SensorsAPIInstance->debug("\nNew API task running on core ");
   SensorsAPIInstance->debug(String(xPortGetCoreID()));
-  //SensorsAPIInstance->_requestSent = false;
+
   while (1) {
-    if(WiFi.status()== WL_CONNECTED && !SensorsAPIInstance->_requestSent) {
-      HTTPClient http;
-
-      http.begin(SensorsAPIInstance->_apiURL);  	
-      http.addHeader("x-api-key", SensorsAPIInstance->_apiKey);
-
-      int startTime = millis();
-      SensorsAPIInstance->debug("Starting request...");
-      int httpResponseCode = http.POST(SensorsAPIInstance->_req);
-      SensorsAPIInstance->debug("Finished in:  ");
-      SensorsAPIInstance->debug(String(millis() - startTime));
-
-      SensorsAPIInstance->debug("response code: ");
-      SensorsAPIInstance->debug(String(httpResponseCode));
-      if(httpResponseCode>0){
-        String response = http.getString(); 
-        Serial.println(response);           //Print request answer
-      }
-     SensorsAPIInstance->_requestSent = true;
+    if (SensorsAPIInstance->_availableData && SensorsAPIInstance->_requestSent) {
+      SensorsAPIInstance->debug("Sending Data...");
+      SensorsAPIInstance->debug(SensorsAPIInstance->_req);
+      SensorsAPIInstance->_requestSent = false;
+      SensorsAPIInstance->_sendAsyncPost();
+      SensorsAPIInstance->debug("Finished sending data");
     } else {
-      if (WiFi.status() != WL_CONNECTED) {
-        Serial.println(WiFi.status());
-
-        Serial.println("Reconnecting to WiFi...");
-        WiFi.disconnect();
-        WiFi.reconnect();
-        //block thread while wifi is reconencted. Otherwise we will keep calling reconnect.
-        while(WiFi.status() != WL_CONNECTED) {
-          delay(200);
-          Serial.print(".");
-        }
-        Serial.println("");
-        Serial.print("Connected to WiFi network with IP Address: ");
-        Serial.println(WiFi.localIP());
-      }
+      //no data to send
+      //SensorsAPIInstance->debug("No data to send...");
     }
-    //wait for the kill. Can't kill task here from here because memory will be not freed.
-    delay(500);
+    delay(100);
+  }
+}
+
+void SensorsAPI::_httpSend() {
+  HTTPClient http;
+
+  http.begin(this->_apiURL);  	
+  http.addHeader("x-api-key", this->_apiKey);
+
+  int startTime = millis();
+  this->debug("Starting request...");
+  int httpResponseCode = http.POST(this->_req);
+  this->debug("Finished in:  ");
+  this->debug(String(millis() - startTime));
+
+  this->debug("response code: ");
+  this->debug(String(httpResponseCode));
+  if(httpResponseCode>0){
+    String response = http.getString(); 
+    Serial.println(response);           //Print request answer
+  }
+}
+
+void SensorsAPI::_sendAsyncPost() {
+  if(WiFi.status()== WL_CONNECTED) {
+    this->_httpSend();
+    this->_availableData = false;
+    this->_requestSent = true;
+
+  } else {
+    if (WiFi.status() != WL_CONNECTED) {
+      this->debug("Reconnecting to WiFi...");
+      WiFi.disconnect();
+      WiFi.reconnect();
+      //block thread while wifi is reconencted. Otherwise we will keep calling reconnect.
+      while(WiFi.status() != WL_CONNECTED) {
+        delay(200);
+        this->debug(".");
+      }
+      this->debug("");
+      this->debug("Reconnected to WiFi network with IP Address: ");
+      this->debug(WiFi.localIP().toString());
+
+      this->_httpSend();
+      this->_availableData = false;
+    }
   }
 }
 
 int SensorsAPI::sendReadings(JsonDocument &req) {
-  this->debug("\nMain task running on core ");
-  this->debug(String(xPortGetCoreID()));
-
-  //delete old task if exists
-  //if (this->taskCreated != 0) {
-    this->deleteRequestTask();
-  //}
-  this->debug("Creating New task");
-  String requestBody;
-  serializeJson(req, requestBody);
-  _req = requestBody;
-
-  taskCreated = millis();
-  xTaskCreatePinnedToCore(
-    this->_sendAsyncPost, /* Task function. */
-    "Task1",   /* name of task. */
-    30000,     /* Stack size of task */
-    (void *)this,      /* parameter of the task */
-    1,         /* priority of the task */
-    &_t1,    /* Task handle to keep track of created task */
-    0
-  );        /* pin task to core 0 */
-  
-  this->debug("Created new task");
-  
-  return 0;
-}
-
-//LEGACY METHOD. First version of method with manual JSON building. Created for tests with single value. Better to use sendReadings instead(). This one is blocking CPU
-int SensorsAPI::sendReading(float tempC) {
-  if(WiFi.status()== WL_CONNECTED) {
-    HTTPClient http;
-
-    http.begin(_apiURL);  	
-    http.addHeader("x-api-key", _apiKey);
-
-    String requestBody1, requestBody2, requestBody3;
-    requestBody1 = String("{\"temp1\": ");
-    requestBody2 = String();
-    requestBody3 = String();
-    requestBody2 = requestBody1 + tempC;
-    requestBody3 = requestBody2 + "}";
-    int httpResponseCode = http.POST(requestBody3);
-
-    this->debug("response code: ");
-    this->debug(String(httpResponseCode));
-    if(httpResponseCode>0){
-      String response = http.getString(); 
-      this->debug(response);
-    }
-
-    return httpResponseCode;
-
+  if (this->_requestSent) {
+    serializeJson(req, _req);
+    this->_availableData = true;
   } else {
-    this->debug("WiFi Disconnected");
+    this->debug("Data not sent. Request not finished.");
   }
   return 0;
 }
--- src/sensorsAPI.h
+++ src/sensorsAPI.h
@@ -9,21 +9,23 @@
 class SensorsAPI {
   public:
     SensorsAPI(const char* apiURL, const char* apiKey, const char* deviceId, TaskHandle_t task1 );
-    int sendReading(float tempC);
     int sendReadings(JsonDocument &req);
-    static void _sendAsyncPost(void* pvParameter);
-    bool isRequestFinished();
-    void deleteRequestTask();
+    
     int taskCreated;
     void enableDebugging();
+    void beginLoop();
+    void _httpSend();
   private:
     void debug(String msg);
     TaskHandle_t _t1;
     const char* _apiURL;
     const char* _apiKey;
     String _req;
-    bool _requestSent;
+    bool _requestSent = true;
     bool _debug;
+    static void _mainLoop(void* pvParameter);
+    bool _availableData = false;
+    void _sendAsyncPost();
 };
 
 #endif
